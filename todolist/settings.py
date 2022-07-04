ae2c7b8326b014715d82a3abb7bd1689f6044a30
ermiasgw <https://github.com/ermiasgw>
2022-07-04 13:37:44 +0300

    second deploy

--- todolist/settings.py
+++ todolist/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
